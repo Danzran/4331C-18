2e6d4d3e49a5321a13b7ee041c6352b411c3051f
mob205 <mobslayer205@gmail.com>
2024-09-02 18:02:53 -0400

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,12 +1,15 @@
-create database CONTACT_MANAGER;
+CREATE DATABASE Contact_Manager;
 
-use CONTACT_MANAGER;
+CREATE USER IF NOT EXISTS ContactAPI IDENTIFIED BY 'apipass';
+
+GRANT ALL ON *.* TO ContactAPI; 
+
+USE Contact_Manager;
 
 CREATE TABLE Logins
 (
 	ID INT NOT NULL AUTO_INCREMENT,
 	DateCreated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	DateLastLoggedIn DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FirstName VARCHAR(50) NOT NULL DEFAULT '',
 	LastName VARCHAR(50) NOT NULL DEFAULT '',
 	Login VARCHAR(50) NOT NULL DEFAULT '',
@@ -21,6 +24,7 @@ CREATE TABLE Contacts
 	LastName VARCHAR(50) NOT NULL DEFAULT '',
 	Email VARCHAR(50) NOT NULL DEFAULT '',
 	Phone VARCHAR(50) NOT NULL DEFAULT '',
+	Address VARCHAR(50) NOT NULL DEFAULT '',
 	User_ID INT NOT NULL DEFAULT 0,
 	PRIMARY KEY (ID),
 	FOREIGN KEY (User_ID) REFERENCES Logins(ID)
@@ -34,9 +38,9 @@ INSERT INTO Logins(FirstName, LastName, Login, Password) VALUES("Bistian", "Biov
 -- INSERT INTO Logins(FirstName, LastName, Login, Password) VALUES(
 
 
-INSERT INTO Contacts(FirstName, LastName, Email, Phone, User_ID) VALUES("Jane", "Schmoe", "jane@theschmoes.com", "521-304-3949", 1);
+INSERT INTO Contacts(FirstName, LastName, Email, Phone, Address, User_ID) VALUES("Jane", "Schmoe", "jane@theschmoes.com", "521-304-3949", "439 Wind Blossom Dr", 1);
 INSERT INTO Contacts(FirstName, LastName, Email, Phone, User_ID) VALUES("Bobby", "Bo Bobbertson", "bobcubed@gmail.com", "439-493-3929", 1);
-INSERT INTO Contacts(FirstName, LastName, Email, Phone, User_ID) VALUES("George", "Marrison", "engineerman458@hotmail.com", "932-493-1929", 2);
+INSERT INTO Contacts(FirstName, LastName, Email, Phone, Address, User_ID) VALUES("George", "Marrison", "engineerman458@hotmail.com", "932-493-1929", "1939 East Blvd", 2);
 INSERT INTO Contacts(FirstName, Email, Phone, User_ID) VALUES("The Beast", "thebeast@gmail.com", "111-222-1234", 2);
-INSERT INTO Contacts(FirstName, LastName, Email, Phone, User_ID) VALUES("Superguy", "McCool", "superguy@gmail.com", "(+5) 129-493-1929", 3);
+INSERT INTO Contacts(FirstName, LastName, Email, Phone, Address, User_ID) VALUES("Kai", "Oslow", "tornado@gmail.com", "(+5) 129-493-1929", "1020 Treehouse Lane", 3);
 INSERT INTO Contacts(FirstName, LastName, Email, Phone, User_ID) VALUES("ajfiwj", "asjiowjfioj", "293i190idk9@eiocji.com", "493-965-4920", 3);
